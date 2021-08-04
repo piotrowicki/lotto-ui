b802a366379332342554ddde21329458d73b26fd
Piotr Nowicki <piotrowicki@gmail.com>
2021-08-04 10:23:51 +0200

    Add Content component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Navigation />
+    <Content />
   </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
+import Content from "./components/Content.vue";
 
 export default {
   name: "App",
   components: {
     Navigation,
+    Content
   },
 };
 </script>
