f80e89d349e2183215cf5769ba899abe834994d8
Piotr Nowicki <piotrowicki@gmail.com>
2021-10-13 18:48:01 +0200

    Try hash history mode to fix url refresh

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import NotFound from "@/views/NotFound.vue";
 import Home from "@/views/Home.vue";
 
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
