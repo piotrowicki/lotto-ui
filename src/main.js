9e67ba51664a9ecf6069f1494a57e50ecdb2afe2
Piotr Nowicki <piotrowicki@gmail.com>
2022-09-13 21:10:58 +0200

    Add logging to keycloak conf

--- src/main.js
+++ src/main.js
@@ -34,7 +34,18 @@ keycloak.init({ onLoad: initOptions.onLoad })
         }
 
         //Token Refresh
-        setInterval(() => { keycloak.updateToken(70) }, 6000)
+        setInterval(() => {
+            keycloak.updateToken(70).then((refreshed) => {
+                if (refreshed) {
+                    console.log('Token refreshed' + refreshed);
+                } else {
+                    console.log('Token not refreshed, valid for '
+                        + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+                }
+            }).catch(() => {
+                console.log('Failed to refresh token');
+            });
+        }, 6000)
 
     }).catch(() => {
         console.log("Authenticated Failed");
