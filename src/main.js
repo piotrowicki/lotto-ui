e4243a185a9eec40d5ed617c59661481a6ac1622
Piotr Nowicki <piotrowicki@gmail.com>
2022-09-13 09:35:02 +0200

    Configure keycloak

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
 import PrimeVue from 'primevue/config'
+import Keycloak from 'keycloak-js'
 
 import 'primevue/resources/themes/saga-blue/theme.css'      //theme
 import 'primevue/resources/primevue.min.css'                //core css
@@ -12,3 +13,29 @@ const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
 app.mount('#app')
+
+let initOptions = {
+    url: 'http://pomocdrogowaostroda.com.pl:8180/', realm: 'lotto-realm', clientId: 'lotto-app', onLoad: 'login-required'
+}
+
+let keycloak = new Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad })
+    .then((auth) => {
+        if (!auth) {
+            window.location.reload();
+        } else {
+            console.log("Authenticated");
+
+            app({
+                el: '#app',
+                render: h => h(app, { props: { keycloak: keycloak } })
+            })
+        }
+
+        //Token Refresh
+        setInterval(() => { keycloak.updateToken(70) }, 6000)
+
+    }).catch(() => {
+        console.log("Authenticated Failed");
+    });
