b38330cdad8f5776741e83182555b2f588e05ec8
Piotr Nowicki <piotrowicki@gmail.com>
2022-10-19 15:49:29 +0200

    Remove primeicons import

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import Keycloak from 'keycloak-js'
 
 import 'primevue/resources/themes/saga-blue/theme.css'      //theme
 import 'primevue/resources/primevue.min.css'                //core css
-// import '/node_modules/primeicons/primeicons.css'            //icons  
 import '/node_modules/primeflex/primeflex.css'              //primeflex
 
 const app = createApp(App);
