3c7f13c2d75aaad20a8c47560ba73de7c326e989
Piotr Nowicki <piotrowicki@gmail.com>
2022-10-19 15:44:37 +0200

    Fix primeicons path

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Keycloak from 'keycloak-js'
 
 import 'primevue/resources/themes/saga-blue/theme.css'      //theme
 import 'primevue/resources/primevue.min.css'                //core css
-import 'primeicons/primeicons.css'                          //icons  
+import '/node_modules/primeicons/primeicons.css'            //icons  
 import '/node_modules/primeflex/primeflex.css'              //primeflex
 
 const app = createApp(App);
